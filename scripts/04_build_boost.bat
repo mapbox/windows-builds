a92ca52fa4e805c7e5d56c28820a228a2baa4b89
bergwerkgis <wb@bergwerk-gis.at>
2014-05-16 09:45:12 +0200

    icu 64bit

--- scripts/04_build_boost.bat
+++ scripts/04_build_boost.bat
@@ -19,8 +19,9 @@ echo.
 pause
 
 ::note for VS2012, use toolset=msvc-11.0 and VS2010 use toolset=msvc-10.0 
+::64bit: http://stackoverflow.com/a/2326485
 echo bjamming ....
-CALL bjam toolset=msvc-12.0 --prefix=..\\%BOOST_PREFIX% --with-python --with-thread --with-filesystem --with-date_time --with-system --with-program_options --with-regex --with-chrono --disable-filesystem2 -sHAVE_ICU=1 -sICU_PATH=%ROOTDIR%\\icu -sICU_LINK=%ROOTDIR%\\icu\\lib\\icuuc.lib release link=static install --build-type=complete
+CALL bjam toolset=msvc-12.0 address-model=%BOOSTADDRESSMODEL% --prefix=..\\%BOOST_PREFIX% --with-python --with-thread --with-filesystem --with-date_time --with-system --with-program_options --with-regex --with-chrono --disable-filesystem2 -sHAVE_ICU=1 -sICU_PATH=%ROOTDIR%\\icu -sICU_LINK=%ROOTDIR%\\icu\\lib\\icuuc.lib release link=static install --build-type=complete
 IF ERRORLEVEL 1 GOTO ERROR
 
 ::if you need python
