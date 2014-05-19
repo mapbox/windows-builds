25553e1dbcf5e880edbb714a473957f8110b063f
bergwerkgis <wb@bergwerk-gis.at>
2014-05-19 09:21:40 +0200

    libpq 64bit

--- scripts/09_build_libpq.bat
+++ scripts/09_build_libpq.bat
@@ -1,6 +1,15 @@
 @echo off
 echo ------ libpq -----
 
+echo If you receive an error about not finding Win32.mak, you may need to do something like:
+echo "set INCLUDE=%include%;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include"
+
+SET INCLUDE=%include%;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+
+
+::http://ftp.postgresql.org/pub/source/
+::http://www.postgresql.org/docs/9.2/static/install-windows.html
+
 CALL bsdtar xvfz "%PKGDIR%\postgresql-%POSTGRESQL_VERSION%.tar.gz"
 IF ERRORLEVEL 1 GOTO ERROR
 
@@ -8,7 +17,11 @@ rename postgresql-%POSTGRESQL_VERSION% postgresql
 IF ERRORLEVEL 1 GOTO ERROR
 cd postgresql\src
 
-CALL nmake /f win32.mak
+echo building ...
+:: !! http://www.postgresql.org/docs/9.0/static/install-windows-libpq.html
+::NMAKE Options: http://msdn.microsoft.com/en-us/library/afyyse50%28v=vs.120%29.aspx
+::NMAKE platform (32/64) used, depends on which Developer command prompt was opened
+IF %BUILDPLATFORM% EQU x64 (CALL nmake /A /B /F win32.mak CPU=AMD64) ELSE (CALL nmake /A /B /F win32.mak)
 IF ERRORLEVEL 1 GOTO ERROR
 
 ::Note: The following errors occurred uring this process:
@@ -18,6 +31,7 @@ IF ERRORLEVEL 1 GOTO ERROR
 
 
 :ERROR
+ECHO ===== ERROR building libpq
 
 cd %ROOTDIR%
 EXIT /b %ERRORLEVEL%
