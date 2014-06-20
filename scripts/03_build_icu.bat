40df8a9b40fb4192c97aa6d01f7e6dbae387d202
bergwerkgis <wb@bergwerk-gis.at>
2014-06-20 15:33:20 +0200

    minor script changes

--- scripts/03_build_icu.bat
+++ scripts/03_build_icu.bat
@@ -9,6 +9,12 @@ ECHO upgrading solution
 CALL devenv.exe /upgrade source\allinone\allinone.sln
 IF ERRORLEVEL 1 GOTO ERROR
 
+
+::!!!!!!!
+::build release AND debug
+::A boost build bug that tests for existence of *debug* version of ICU even when building release only version of boost. 
+::http://devwiki.neosys.com/index.php/Building_Boost_32/64_on_Windows
+
 ECHO building ...
 CALL msbuild source\allinone\allinone.sln /t:Rebuild  /p:Configuration="Release" /p:Platform=%BUILDPLATFORM%
 IF ERRORLEVEL 1 GOTO ERROR
--- scripts/16_build_gdal.bat
+++ scripts/16_build_gdal.bat
@@ -14,20 +14,22 @@ CALL move gdal-%GDAL_VERSION% gdal/gdal
 IF ERRORLEVEL 1 GOTO ERROR
 
 echo.
+echo clone from: https://github.com/OSGeo/gdal
+echo.
 echo !To get KML,GPX, GeoRSS support!
 echo !edit the 'gdal/gdal/nmake.opt' to point to the location the expat binary was installed to:!
 echo.
 echo When compiling 64bit download libexpat dev packages from http://www.gtk.org/download/win64.php
 ::echo Also un-comment WIN64=YES in nmake.opt -> can be passed as argument, see below
 echo.
-echo EXPAT_DIR="C:\Expat2.1.0"
+echo EXPAT_DIR=C:\Expat2.1.0
 echo un-comment the other two lines as well
 echo EXPAT_INCLUDE = -I$(EXPAT_DIR)/source/lib
 echo EXPAT_LIB = $(EXPAT_DIR)/bin/libexpat.lib
 echo.
 echo If there is an error about missing 'expat.h' change
 echo <include expat.h> to
-echo #include "C:\Expat 2.1.0\Source\lib\expat.h"
+echo #include "C:\Expat2.1.0\Source\lib\expat.h"
 echo gdal/gdal/ogr/ogr_expat.h
 echo.
 
@@ -40,20 +42,30 @@ cd gdal/gdal
 ::see basic options
 
 
-CALL nmake /F makefile.vc clean
-IF ERRORLEVEL 1 GOTO ERROR
 
 ::MSVC_VER compiler version
 ::http://stackoverflow.com/a/2676904
 ::VS2010 MSC_VER=1600
 ::VS2012 MSC_VER=1700
 ::VS2013 MSC_VER=1800
+
+
+:: !!! BUILD FIRST and then do 'devinstall'!!!
+
 IF %BUILDPLATFORM% EQU x64 (
+	CALL nmake /F makefile.vc clean WIN64=YES
+	IF ERRORLEVEL 1 GOTO ERROR
 	CALL nmake /A /F makefile.vc MSVC_VER=1800 WIN64=YES
+	IF ERRORLEVEL 1 GOTO ERROR
+	:: nmake /F makefile.vc devinstall WIN64=YES MSVC_VER=1800 GDAL_HOME=C:\dev2\mapnik-dependencies\64_gdal
 ) ELSE (
+	CALL nmake /F makefile.vc clean
+	IF ERRORLEVEL 1 GOTO ERROR
 	CALL nmake /A /F makefile.vc MSVC_VER=1800
+	IF ERRORLEVEL 1 GOTO ERROR
+	:: nmake /F makefile.vc devinstall MSVC_VER=1800 GDAL_HOME=C:\dev2\mapnik-dependencies\32_gdal
 )
-IF ERRORLEVEL 1 GOTO ERROR
+
 
 ::ECHO upgrading solution
 ::CALL devenv.exe /upgrade makegdal10.sln
