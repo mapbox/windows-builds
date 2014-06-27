c0c44c877da55ea3dd7a740889e36d8de670b489
Dane Springmeyer <dane@mapbox.com>
2014-06-26 19:27:16 -0700

    boost - quiet main build + pass toolset to boostrap

--- scripts/04_build_boost.bat
+++ scripts/04_build_boost.bat
@@ -41,7 +41,7 @@ echo using python : 2.7 : C:/Python27/python.exe ; >> user-config.jam
 
 if NOT EXIST b2.exe (
   echo calling bootstrap bat
-  CALL bootstrap.bat
+  CALL bootstrap.bat --with-toolset=msvc-12.0
   IF ERRORLEVEL 1 GOTO ERROR
 )
 
@@ -74,7 +74,7 @@ IF ERRORLEVEL 1 GOTO ERROR
 :: cat bin.v2/config.log to see problems
 
 ::CALL b2 toolset=msvc-12.0 --clean
-CALL b2 -d1 toolset=msvc-12.0 --reconfigure -q address-model=%BOOSTADDRESSMODEL% --prefix=..\\%BOOST_PREFIX% --with-python --with-thread --with-filesystem --with-date_time --with-system --with-program_options --with-regex --with-chrono --disable-filesystem2 -sHAVE_ICU=1 -sICU_PATH=%PKGDIR%\\icu -sICU_LINK=%PKGDIR%\\icu\\lib\\icuuc.lib release link=static install --build-type=complete
+CALL b2 -d0 toolset=msvc-12.0 --reconfigure -q address-model=%BOOSTADDRESSMODEL% --prefix=..\\%BOOST_PREFIX% --with-python --with-thread --with-filesystem --with-date_time --with-system --with-program_options --with-regex --with-chrono --disable-filesystem2 -sHAVE_ICU=1 -sICU_PATH=%PKGDIR%\\icu -sICU_LINK=%PKGDIR%\\icu\\lib\\icuuc.lib release link=static install --build-type=complete
 ::icu: lib64\
 :: -a rebuild everything
 :: -q stop at first error
