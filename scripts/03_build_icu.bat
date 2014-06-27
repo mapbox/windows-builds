059190c0acd71474e7dcc4063f0e0aefaadbb2b9
Dane Springmeyer <dane@mapbox.com>
2014-06-26 20:01:57 -0700

    parallel builds

--- scripts/03_build_icu.bat
+++ scripts/03_build_icu.bat
@@ -21,7 +21,7 @@ if NOT EXIST icu (
 cd icu
 IF ERRORLEVEL 1 GOTO ERROR
 
-call msbuild source\allinone\allinone.sln /toolsversion:12.0 /p:PlatformToolset=v120 /p:Configuration="Release" /p:Platform=%BUILDPLATFORM%
+call msbuild /m source\allinone\allinone.sln /toolsversion:12.0 /p:PlatformToolset=v120 /p:Configuration="Release" /p:Platform=%BUILDPLATFORM%
 IF ERRORLEVEL 1 GOTO ERROR
 
 ::echo building release AND debug
