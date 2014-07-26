aa91896b70e80a8a8307dc780e16f055acff2a93
Dane Springmeyer <dane@mapbox.com>
2014-07-25 19:24:09 -0700

    fix libtiff build

--- scripts/10_build_tiff.bat
+++ scripts/10_build_tiff.bat
@@ -22,22 +22,10 @@ if NOT EXIST libtiff (
 cd libtiff
 IF ERRORLEVEL 1 GOTO ERROR
 
-set P1=s/\^#JPEG_SUPPORT.*/JPEG_SUPPORT = 1/;
-set P2=s/\^#JPEGDIR.*/JPEGDIR = %ROOTDIR:\=\\\%\\\jpeg/;
-set P3=s/\^#JPEG_INCLUDE/JPEG_INCLUDE/;
-set P4=s/\^#JPEG_LIB.*/JPEG_LIB = \$(JPEGDIR^)\\\libjpeg.lib/;
-set P5=s/\^#ZIP_SUPPORT.*/ZIP_SUPPORT = 1/;
-set P6=s/\^#ZLIBDIR.*/ZLIBDIR = %ROOTDIR:\=\\\%\\\zlib-1.2.5/;
-set P7=s/\^#ZLIB_INCLUDE/ZLIB_INCLUDE/;
-set P8=s/\^#ZLIB_LIB.*/ZLIB_LIB = \$(ZLIBDIR^)\\\zlib.lib/;
-
-set PATTERN="%P1%%P2%%P3%%P4%%P5%%P6%%P7%%P8%"
-
-CALL sed %PATTERN%  nmake.opt > nmake.opt.fixed
-IF ERRORLEVEL 1 GOTO ERROR
-
-CALL move /Y nmake.opt.fixed nmake.opt
-IF ERRORLEVEL 1 GOTO ERROR
+echo JPEG_SUPPORT = 1 >> nmake.opt
+echo JPEGDIR = %PKGDIR%\jpeg >> nmake.opt
+echo JPEG_INCLUDE   = -I$(JPEGDIR) >> nmake.opt
+echo JPEG_LIB   = $(JPEGDIR)/Release/jpeg.lib >> nmake.opt
 
 ::http://www.remotesensing.org/libtiff/build.html#PC
 
@@ -46,7 +34,8 @@ CALL nmake /F Makefile.vc clean
 IF ERRORLEVEL 1 GOTO ERROR
 
 echo building ....
-CALL nmake /A /F Makefile.vc >%ROOTDIR%\build_tiff-%TIFF_VERSION%.log 2>&1
+CALL nmake /A /F Makefile.vc
+:: >%ROOTDIR%\build_tiff-%TIFF_VERSION%.log 2>&1
 IF ERRORLEVEL 1 GOTO ERROR
 
 GOTO DONE
