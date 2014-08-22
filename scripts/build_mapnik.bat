ab896b8cce7583196c4e43181b1dabdadb5b5090
Dane Springmeyer <dane@mapbox.com>
2014-08-21 17:58:17 -0700

    defer most work to mapnik build

--- scripts/build_mapnik.bat
+++ scripts/build_mapnik.bat
@@ -16,32 +16,15 @@ git fetch
 IF ERRORLEVEL 1 GOTO ERROR
 git checkout gyp
 IF ERRORLEVEL 1 GOTO ERROR
+git pull
+IF ERRORLEVEL 1 GOTO ERROR
 
 if NOT EXIST gyp (
-    git clone https://chromium.googlesource.com/external/gyp.git gyp
+    CALL git clone https://chromium.googlesource.com/external/gyp.git gyp
 )
 
-ECHO checking file valid ...
-CALL gyp\gyp.bat mapnik.gyp --depth=. --check --no-duplicate-basename-check
-IF ERRORLEVEL 1 GOTO ERROR
-
-ECHO checking file valid ...
-CALL gyp\gyp.bat mapnik.gyp --depth=. ^
- -Dincludes=../mapnik-sdk/includes ^
- -Dlibs=../mapnik-sdk/libs ^
- -f msvs -G msvs_version=2013 ^
- --generator-output=build ^
- --no-duplicate-basename-check
-IF ERRORLEVEL 1 GOTO ERROR
-
-::ECHO deleting duplicate solution files ...
-::DEL *.vcxproj*
-::IF ERRORLEVEL 1 GOTO ERROR
-::DEL *.sln
-::IF ERRORLEVEL 1 GOTO ERROR
-
 ECHO building mapnik
-build.bat
+call build.bat
 IF ERRORLEVEL 1 GOTO ERROR
 
 GOTO DONE
