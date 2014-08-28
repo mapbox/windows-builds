b7cc8765f7d9ac5f9a647079eb968cb87569e752
Dane Springmeyer <dane@mapbox.com>
2014-08-28 10:31:36 -0700

    actually apply patch for gdal

--- scripts/build_gdal.bat
+++ scripts/build_gdal.bat
@@ -22,6 +22,8 @@ if NOT EXIST gdal (
 cd gdal
 IF ERRORLEVEL 1 GOTO ERROR
 
+patch -N -p1 < %PATCHES%/gdal.diff || true
+
 ::echo When compiling 64bit download libexpat dev packages from http://www.gtk.org/download/win64.php
 ::echo Also un-comment WIN64=YES in nmake.opt -> can be passed as argument, see below
 
