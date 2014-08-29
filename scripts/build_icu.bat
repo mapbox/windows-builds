cb934164d2ecf7955f04332a3e9e2eb0ceb72567
Dane Springmeyer <dane@mapbox.com>
2014-08-29 00:02:28 -0700

    patch icu to use uint16_t UChar

--- scripts/build_icu.bat
+++ scripts/build_icu.bat
@@ -24,6 +24,8 @@ if NOT EXIST icu (
 cd icu
 IF ERRORLEVEL 1 GOTO ERROR
 
+patch -N -p1 < %PATCHES%/icu3.diff || true
+
 call msbuild source\allinone\allinone.sln /m /target:common;makedata;i18n /toolsversion:%TOOLS_VERSION% /p:PlatformToolset=%PLATFORM_TOOLSET% /p:Configuration="Release" /p:Platform=%BUILDPLATFORM%
 IF ERRORLEVEL 1 GOTO ERROR
 
