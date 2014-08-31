78018fe6f150070120bb34ce5c882bd838ef6867
Dane Springmeyer <dane@mapbox.com>
2014-08-31 15:48:37 +0000

    shuffle build order

--- scripts/build.bat
+++ scripts/build.bat
@@ -32,31 +32,31 @@ pause
 ::     problems with 64, maybe use precompiled 64bit: http://sourceforge.net/projects/boost/files/boost-binaries/1.55.0-build2/
 ::  FREETYPE: branch for copying for lib depending on platform
 
-CALL scripts\build_icu.bat
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-CALL scripts\build_boost.bat
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-CALL scripts\build_jpeg.bat
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-CALL scripts\build_webp.bat
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-CALL scripts\build_freetype.bat
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
 CALL scripts\build_zlib.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 CALL scripts\build_libpng.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-CALL scripts\build_libpq.bat
+CALL scripts\build_jpeg.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 CALL scripts\build_tiff.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+CALL scripts\build_jpeg.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+CALL scripts\build_icu.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+CALL scripts\build_boost.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+CALL scripts\build_webp.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+CALL scripts\build_freetype.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 CALL scripts\build_proj4.bat
@@ -83,11 +83,17 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 CALL scripts\build_gdal.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+CALL scripts\build_libpq.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
 CALL scripts\build_harfbuzz.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-::CALL scripts\build_mapnik.bat
-::IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+CALL scripts\build_mapnik.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+CALL scripts\build_node_mapnik.bat
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 ::GEOS not working
 ::CALL scripts\build_geos.bat
