18f9a30e584f6f721c7b72a7f72eb962ccec06f0
Dane Springmeyer <dane@mapbox.com>
2014-09-05 01:47:56 +0000

    Adapt to mapnik 3.x master branch

--- scripts/build_mapnik.bat
+++ scripts/build_mapnik.bat
@@ -9,14 +9,21 @@ IF "%ROOTDIR%"=="" ( echo "ROOTDIR variable not set" && GOTO DONE )
 cd %PKGDIR%
 if NOT EXIST mapnik-3.x (
     git clone https://github.com/mapnik/mapnik mapnik-3.x
+    IF ERRORLEVEL 1 GOTO ERROR
 )
 cd mapnik-3.x
 IF ERRORLEVEL 1 GOTO ERROR
 git fetch
 IF ERRORLEVEL 1 GOTO ERROR
-git checkout 3x-msvs
-IF ERRORLEVEL 1 GOTO ERROR
 git pull
+IF ERRORLEVEL 1 GOTO ERROR
+
+if NOT EXIST mapnik-3.x (
+    git clone https://github.com/mapnik/mapnik-gyp mapnik-gyp
+    IF ERRORLEVEL 1 GOTO ERROR
+)
+
+cd mapnik-gyp
 IF ERRORLEVEL 1 GOTO ERROR
 
 ECHO building mapnik
