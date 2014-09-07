0500a5f8c33a658c5acf1235ff0addd91519d75b
Dane Springmeyer <dane@mapbox.com>
2014-09-07 00:06:17 +0000

    cleanup

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,36 +2,6 @@
 
 set STARTTIME=%TIME%
 
-echo "use this script only from"
-echo     "'VS2013 x64 Native Tools Command Prompt'"
-echo "or"
-echo     "'VS2013 x86 Native Tools Command Prompt'"
-echo "depending on your needs"
-echo "call from %ROOTDIR%"
-echo.
-echo "ATTENTION! boost x64 build needs to be started from x86 command prompt"
-echo.
-
-pause
-
-
-::INSTALL Python 64
-::http://stackoverflow.com/a/12448411 and https://medium.com/learning-to-code/da577d9c436b
-::This appears to be working for me on Windows 7 64 bit. 
-::Choose one version to be your default installation, e.g. 64 bit, and install it first.
-::Before doing anything else install the other version.
-::Specify a different installation directory and in the Customize Python 2.7.3 screen 
-::select Register Extensions and select Entire feature will be unavailable.
-
-
-::TODO:
-::  ICU: adjust paths to icu for 64: lib64
-::  BOOST:
-::     check for 32/64bit within build_boost.bat
-::     adjust Python Path 32/64bit
-::     problems with 64, maybe use precompiled 64bit: http://sourceforge.net/projects/boost/files/boost-binaries/1.55.0-build2/
-::  FREETYPE: branch for copying for lib depending on platform
-
 CALL scripts\build_zlib.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -97,11 +67,6 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 CALL scripts\build_node_mapnik.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-::GEOS not working
-::CALL scripts\build_geos.bat
-::IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
 
 GOTO DONE
 
