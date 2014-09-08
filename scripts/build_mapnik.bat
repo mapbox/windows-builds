18e43c4e10712b27423f5575703c848c25d34e55
Dane Springmeyer <dane@mapbox.com>
2014-09-07 21:08:22 -0700

    Also put 'Scripts' dir on path so that pip works

--- scripts/build_mapnik.bat
+++ scripts/build_mapnik.bat
@@ -31,8 +31,8 @@ git pull
 IF ERRORLEVEL 1 GOTO ERROR
 
 ECHO building mapnik
-if "%BOOSTADDRESSMODEL%"=="32" if EXIST %ROOTDIR%\tmp-bin\python2-x86-32 SET PATH=%ROOTDIR%\tmp-bin\python2-x86-32;%PATH%
-if "%BOOSTADDRESSMODEL%"=="64" if EXIST %ROOTDIR%\tmp-bin\python2 SET PATH=%ROOTDIR%\tmp-bin\python2;%PATH%
+if "%BOOSTADDRESSMODEL%"=="32" if EXIST %ROOTDIR%\tmp-bin\python2-x86-32 SET PATH=%ROOTDIR%\tmp-bin\python2-x86-32;%ROOTDIR%\tmp-bin\python2-x86-32\Scripts;%PATH%
+if "%BOOSTADDRESSMODEL%"=="64" if EXIST %ROOTDIR%\tmp-bin\python2 SET PATH=%ROOTDIR%\tmp-bin\python2;%ROOTDIR%\tmp-bin\python2\Scripts;%PATH%
 call build.bat
 IF ERRORLEVEL 1 GOTO ERROR
 
