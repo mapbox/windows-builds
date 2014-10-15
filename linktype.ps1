8699efb80ff8f73f20e3641e2554b2da6bf7c49e
bergwerkgis <wb@bergwerk-gis.at>
2014-10-15 15:06:52 +0200

    also check DLLs

--- linktype.ps1
+++ linktype.ps1
@@ -1,5 +1,17 @@
+Write-Host 'Checking for files linked with /MT or /MTd';
+
+Write-Host 'checking libs...';
 
 foreach ($item in Get-ChildItem $args[0] -Recurse -Include *.lib){
+    $output = dumpbin /DIRECTIVES $item.FullName;
+    if ($output -like '*LIBCMT*') {
+        Write-Host $item.FullName;
+    }
+}
+
+Write-Host 'checking dlls...';
+
+foreach ($item in Get-ChildItem $args[0] -Recurse -Include *.dll){
     $output = dumpbin /DIRECTIVES $item.FullName;
     if ($output -like '*LIBCMT*') {
         Write-Host $item.FullName;
