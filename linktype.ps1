25eeca7ed6fc62a5488d082b47a8891af2982ecd
bergwerkgis <wb@bergwerk-gis.at>
2014-10-15 15:19:31 +0200

    also look for /MDd

--- linktype.ps1
+++ linktype.ps1
@@ -1,11 +1,16 @@
-Write-Host 'Checking for files linked with /MT or /MTd';
+Write-Host 'Checking for files linked with /MT, /MTd or /MDd';
 
 Write-Host 'checking libs...';
 
 foreach ($item in Get-ChildItem $args[0] -Recurse -Include *.lib){
     $output = dumpbin /DIRECTIVES $item.FullName;
+    #MT and MTd
     if ($output -like '*LIBCMT*') {
-        Write-Host $item.FullName;
+        Write-Host "MT or MTd $item";
+    }
+    #MDd
+    if ($output -like '*msvcrtd*') {
+        Write-Host "MDd $item";
     }
 }
 
@@ -13,8 +18,13 @@ Write-Host 'checking dlls...';
 
 foreach ($item in Get-ChildItem $args[0] -Recurse -Include *.dll){
     $output = dumpbin /DIRECTIVES $item.FullName;
+    #MT and MTd
     if ($output -like '*LIBCMT*') {
-        Write-Host $item.FullName;
+        Write-Host "MT or MTd $item";
+    }
+    #MDd
+    if ($output -like '*msvcrtd*') {
+        Write-Host "MDd $item";
     }
 }
 
