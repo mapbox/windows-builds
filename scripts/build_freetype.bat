d48c100bde1ca8a16d6ea9b5d24837df1d6693d1
bergwerkgis <wb@bergwerk-gis.at>
2014-10-17 15:20:59 +0200

    debug: freetype

--- scripts/build_freetype.bat
+++ scripts/build_freetype.bat
@@ -44,11 +44,26 @@ msbuild ^
 IF ERRORLEVEL 1 GOTO ERROR
 
 IF %BUILDPLATFORM% EQU x64 (
+  IF %BUILD_TYPE% EQU Release (
     CALL copy /Y builds\windows\vc2010\x64\Release\freetype253.lib freetype.lib
+    IF ERRORLEVEL 1 GOTO ERROR
+  ) ELSE (
+    CALL copy /Y objs\win64\vc2010\freetype253_D.lib freetype.lib
+    IF ERRORLEVEL 1 GOTO ERROR
+    CALL copy /Y objs\debug\freetype.pdb freetype.pdb
+    IF ERRORLEVEL 1 GOTO ERROR
+  )
 ) ELSE (
+  IF %BUILD_TYPE% EQU Release (
     CALL copy /Y objs\win32\vc2010\freetype253.lib freetype.lib
+    IF ERRORLEVEL 1 GOTO ERROR
+  ) ELSE (
+    CALL copy /Y objs\win32\vc2010\freetype253_D.lib freetype.lib
+    IF ERRORLEVEL 1 GOTO ERROR
+    CALL copy /Y objs\debug\freetype.pdb freetype.pdb
+    IF ERRORLEVEL 1 GOTO ERROR
+  )
 )
-IF ERRORLEVEL 1 GOTO ERROR
 
 GOTO DONE
 
