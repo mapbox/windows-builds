cee8940905ba44d38eafa5afbb10f2c12a29fe48
bergwerkgis <wb@bergwerk-gis.at>
2015-01-07 22:42:36 +0100

    [wip] libosmium, cmake needs forward slashes in path to bzip2, any ideas????

--- scripts/build_libosmium.bat
+++ scripts/build_libosmium.bat
@@ -29,17 +29,22 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 cmake .. ^
 -DBOOST_ROOT=%PKGDIR%\boost ^
--DOSMPBF_LIBRARY=C:\mb\mapnik-dependencies-64\packages\OSM-binary\deploy\lib\osmpbf.lib ^
--DOSMPBF_INCLUDE_DIR=C:\mb\mapnik-dependencies-64\packages\OSM-binary\deploy\include ^
+-DOSMPBF_LIBRARY=%PKGDIR%\OSM-binary\deploy\lib\osmpbf.lib ^
+-DOSMPBF_INCLUDE_DIR=%PKGDIR%\packages\OSM-binary\deploy\include ^
 -DPROTOBUF_LIBRARY=%PKGDIR%\protobuf\vsprojects\%BUILDPLATFORM%\%BUILD_TYPE%\libprotobuf-lite.lib ^
 -DPROTOBUF_INCLUDE_DIR=%PKGDIR%\protobuf\src ^
--DZLIB_LIBRARY=C:\mb\mapnik-dependencies-64\packages\zlib\zlib.lib ^
--DZLIB_INCLUDE_DIR=C:\mb\mapnik-dependencies-64\packages\zlib ^
--DEXPAT_LIBRARY=C:\mb\mapnik-dependencies-64\packages\expat\win32\bin\Release\libexpat.lib ^
--DEXPAT_INCLUDE_DIR=C:\mb\mapnik-dependencies-64\packages\expat\lib ^
--DBZIP2_LIBRARIES=C:\mb\mapnik-dependencies-64\packages\bzip2\libbz2.lib ^
--DBZIP2_INCLUDE_DIR=C:\mb\mapnik-dependencies-64\packages\bzip2
+-DZLIB_LIBRARY=%PKGDIR%\zlib\zlib.lib ^
+-DZLIB_INCLUDE_DIR=%PKGDIR%\zlib ^
+-DEXPAT_LIBRARY=%PKGDIR%\expat\win32\bin\Release\libexpat.lib ^
+-DEXPAT_INCLUDE_DIR=%PKGDIR%\expat\lib ^
+-DBZIP2_LIBRARIES="C:\\mb\\mapnik-dependencies-64\\packages\\bzip2\\libbz2.lib" ^
+-DBZIP2_INCLUDE_DIR=%PKGDIR%\bzip2
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+TO SOLVE
+libbz2 needs forward slashes to work, otherwise ERROR
+ Looking for BZ2_bzCompressInit in C:\mb\mapnik-dependencies-64\packages\bzip2\libbz2.lib
+ Invalid escape sequence \m
 
 nmake
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
