48ddf994bcd756562a224dd6f7e03804d347681c
bergwerkgis <wb@bergwerk-gis.at>
2015-02-19 12:35:57 +0000

    different paths for 32bit debug symbols

--- scripts/package_mapnik_debug_symbols.ps1
+++ scripts/package_mapnik_debug_symbols.ps1
@@ -16,6 +16,7 @@ $needed_env_vars = @(
     , 'PLATFORMX'
     , 'LIBPNG_VERSION_FILE'
     , 'BUILDPLATFORM'
+    , 'VERBOSE'
 )
 
 
@@ -25,13 +26,29 @@ $dest_sdk_input = "$env:PKGDIR\mapnik-$env:MAPNIKBRANCH\mapnik-gyp\mapnik-sdk\li
 $src_mapnik_lib = "$env:PKGDIR\mapnik-$env:MAPNIKBRANCH\mapnik-gyp\build\$env:BUILD_TYPE\"
 $pkg_dir="$env:PKGDIR\"
 
+$protoc_pdb = "protobuf\vsprojects\$env:BUILDPLATFORM\$env:BUILD_TYPE\"
+$icu_in_uc_pdb = "icu\lib$env:TARGET_ARCH\"
+$expat_pdb = "expat\$env:BUILDPLATFORM\$env:BUILD_TYPE\"
+$jpeg_pdb = "jpeg\$env:PLATFORMX\Release\"
+$png_version_pdb = "libpng\projects\vstudio\$env:PLATFORMX\$env:BUILD_TYPE\"
+$proto_pdb = "protobuf\vsprojects\$env:BUILDPLATFORM\$env:BUILD_TYPE\"
+
+if($env:TARGET_ARCH -eq 32){
+    $protoc_pdb = "protobuf\vsprojects\$env:BUILD_TYPE\"
+    $icu_in_uc_pdb = "icu\lib\"
+    $expat_pdb = "expat\$env:BUILDPLATFORM\bin\$env:BUILD_TYPE\"
+    $jpeg_pdb = "jpeg\Release\"
+    $png_version_pdb = "libpng\projects\vstudio\$env:BUILD_TYPE\"
+    $proto_pdb = "protobuf\vsprojects\$env:BUILD_TYPE\"
+}
+
 
 # hashtable with destinations and corresponding files in arrays
 $file_list = @{
     "$dest_sdk_bin" = @(
         ("$src_mapnik_lib" + "nik2img.pdb"),
         ("$src_mapnik_lib" + "shapeindex.pdb"),
-        ("$pkg_dir" + "protobuf\vsprojects\$env:BUILDPLATFORM\$env:BUILD_TYPE\protoc.pdb")
+        ("$pkg_dir" + $protoc_pdb + "protoc.pdb")
     )
     "$dest_sdk_lib" = @(
         ("$src_mapnik_lib" + "_mapnik.pdb"),
@@ -41,24 +58,24 @@ $file_list = @{
         ("$pkg_dir" + "cairo\src\$env:BUILD_TYPE\cairo.pdb"),
         ("$pkg_dir" + "gdal\gdal$env:GDAL_VERSION_FILE.pdb"),
         ("$pkg_dir" + "icu\source\stubdata\$env:PLATFORMX\$env:BUILD_TYPE\icudt.pdb"),
-        ("$pkg_dir" + "icu\lib$env:TARGET_ARCH\icuin.pdb"),
-        ("$pkg_dir" + "icu\lib$env:TARGET_ARCH\icuuc.pdb"),
-        ("$pkg_dir" + "expat\$env:PLATFORMX\Release\expat.pdb"),
-        ("$pkg_dir" + "jpeg\$env:PLATFORMX\Release\jpeg.pdb"),
+        ("$pkg_dir" + $icu_in_uc_pdb + "icuin.pdb"),
+        ("$pkg_dir" + $icu_in_uc_pdb + "icuuc.pdb"),
+        ("$pkg_dir" + $expat_pdb + "expat.pdb"),
+        ("$pkg_dir" + $jpeg_pdb + "jpeg.pdb"),
         ("$pkg_dir" + "jpeg\libjpeg.pdb"),
         ("$pkg_dir" + "libpng\libpng.pdb"),
-        ("$pkg_dir" + "libpng\projects\vstudio\$env:PLATFORMX\$env:BUILD_TYPE\libpng$env:LIBPNG_VERSION_FILE.pdb"),
-        ("$pkg_dir" + "postgresql\src\interfaces\libpq\$env:BUILD_TYPE\libpq.dll"),
-        ("$pkg_dir" + "protobuf\vsprojects\$env:BUILDPLATFORM\$env:BUILD_TYPE\libprotoc.pdb"),
-        ("$pkg_dir" + "protobuf\vsprojects\$env:BUILDPLATFORM\$env:BUILD_TYPE\libprotobuf.pdb"),
-        ("$pkg_dir" + "protobuf\vsprojects\$env:BUILDPLATFORM\$env:BUILD_TYPE\libprotobuf-lite.pdb"),
+        ("$pkg_dir" + $png_version_pdb + "libpng$env:LIBPNG_VERSION_FILE.pdb"),
+        ("$pkg_dir" + "postgresql\src\interfaces\libpq\$env:BUILD_TYPE\libpqdll.pdb"),
+        ("$pkg_dir" + $proto_pdb + "libprotoc.pdb"),
+        ("$pkg_dir" + $proto_pdb + "libprotobuf.pdb"),
+        ("$pkg_dir" + $proto_pdb + "libprotobuf-lite.pdb"),
         ("$pkg_dir" + "libtiff\libtiff\libtiff.pdb"),
-        ("$pkg_dir" + "webp\output\$env:BUILD_TYPE-dynamic\$env:BUILDPLATFORM\lib\libwebp_dll.pdb"),
+        ("$pkg_dir" + "webp\output\$env:BUILD_TYPE-dynamic\$env:PLATFORMX\lib\libwebp_dll.pdb"),
         ("$pkg_dir" + "libxml2\win32\bin.msvc\libxml2.pdb"),
         ("$pkg_dir" + "proj\src\proj.pdb"),
         ("$pkg_dir" + "sqlite\sqlite3.pdb"),
-        ("$pkg_dir" + "zlib\contrib\vstudio\vc11\$env:BUILDPLATFORM\ZlibStat" + $env:BUILD_TYPE + "WithoutAsm\zlibstat.pdb"),
-        ("$pkg_dir" + "zlib\contrib\vstudio\vc11\$env:BUILDPLATFORM\ZlibDll" + $env:BUILD_TYPE + "WithoutAsm\zlibwapi.pdb")
+        ("$pkg_dir" + "zlib\contrib\vstudio\vc11\$env:PLATFORMX\ZlibStat" + $env:BUILD_TYPE + "WithoutAsm\zlibstat.pdb"),
+        ("$pkg_dir" + "zlib\contrib\vstudio\vc11\$env:PLATFORMX\ZlibDll" + $env:BUILD_TYPE + "WithoutAsm\zlibwapi.pdb")
     )
     "$dest_sdk_input" = @(
         ("$src_mapnik_lib" + "csv.pdb"),
@@ -93,8 +110,14 @@ Function copy-all-files(){
     $err_files = @()
 
     $file_list.GetEnumerator() | % {
+        if($env:VERBOSE -eq 1){
+            Write-Host $nl$nl "---->" $_.Key $nl -ForegroundColor Blue
+        }
         foreach($file in $_.Value){
             try {
+                if($env:VERBOSE -eq 1){
+                    Write-Host $file
+                }
                 Copy-Item $file $_.Key -Force
                 $script:cnt_success++
             }
