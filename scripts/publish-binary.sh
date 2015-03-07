57bb963c794a8fdf0108fa1fe10c160d58da4244
bergwerkgis <wb@bergwerk-gis.at>
2015-03-07 14:09:59 +0100

    typo in AWS key  [publish binary]

--- scripts/publish-binary.sh
+++ scripts/publish-binary.sh
@@ -26,7 +26,7 @@ maxtimeout=2880
 user_data="<powershell>
     ([ADSI]\"WinNT://./Administrator\").SetPassword(\"Diogenes1234\")
     \$env:PUBLISHMAPNIKSDK=${PUBLISH_SDK}
-    \$env:AWS_ACCESS_KEY_ID?${PUBLISH_KEY}
+    \$env:AWS_ACCESS_KEY_ID=${PUBLISH_KEY}
     \$env:AWS_SECRET_ACCESS_KEY=${PUBLISH_ACCESS}
     Invoke-WebRequest https://mapnik.s3.amazonaws.com/dist/dev/windows-build-server/build.ps1 -OutFile Z:\\build.ps1
     & Z:\\build.ps1
