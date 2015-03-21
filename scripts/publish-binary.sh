6d626198b179d0d1b46da2f9bd930ce3ff0edd46
bergwerkgis <wb@bergwerk-gis.at>
2015-03-21 15:13:06 +0100

    leave machine on [publish debug], don't build, but clone, write setting with [publish binary]

--- scripts/publish-binary.sh
+++ scripts/publish-binary.sh
@@ -4,7 +4,7 @@ set -e
 
 
 PUBLISH_SDK=0
-BUILD_CMD="& Z:\\build.ps1"
+BUILD_CMD="wbs-build.ps1"
 STOP_COMPUTER="Stop-Computer"
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 echo $COMMIT_MESSAGE
@@ -18,7 +18,7 @@ fi
 if test "${COMMIT_MESSAGE#*'[publish debug]'}" != "$COMMIT_MESSAGE"
 then
     echo "Commit includes [publish debug] skipping stack teardown."
-    BUILD_CMD=""
+    BUILD_CMD="wbs-build-prepare.ps1"
     STOP_COMPUTER=""
 fi
 
@@ -39,8 +39,8 @@ user_data="<powershell>
     \$env:PUBLISHMAPNIKSDK=${PUBLISH_SDK};
     \$env:AWS_ACCESS_KEY_ID=\"${PUBLISH_KEY}\";
     \$env:AWS_SECRET_ACCESS_KEY=\"${PUBLISH_ACCESS}\";
-    Invoke-WebRequest https://mapnik.s3.amazonaws.com/dist/dev/windows-build-server/build.ps1 -OutFile Z:\\build.ps1;
-    ${BUILD_CMD}
+    Invoke-WebRequest https://mapnik.s3.amazonaws.com/dist/dev/windows-build-server/${BUILD_CMD} -OutFile Z:\\${BUILD_CMD};
+    & Z:\\${BUILD_CMD}
     ${STOP_COMPUTER}
     </powershell>
     <persist>true</persist>"
---
+++ scripts/wbs-build-prepare.ps1
@@ -0,0 +1,20 @@
+﻿
+Write-Host "cloning repos"
+& "C:\Program Files (x86)\Git\bin\git" clone https://github.com/mapbox/windows-builds.git Z:\mb\windows-builds-32
+& "C:\Program Files (x86)\Git\bin\git" clone https://github.com/mapbox/windows-builds.git Z:\mb\windows-builds-64
+
+Write-Host "PUBLISHMAPNIKSDK: $env:PUBLISHMAPNIKSDK"
+if (-not(Test-Path "Env:\PUBLISHMAPNIKSDK")){
+    $publish_mapnik_sdk='PUBLISHMAPNIKSDK=0'
+} else {
+    $publish_mapnik_sdk="PUBLISHMAPNIKSDK=$env:PUBLISHMAPNIKSDK"
+}
+
+Write-Host "writing config"
+"settings ""TARGET_ARCH=32"" ""IGNOREFAILEDTESTS=1"" ""PACKAGEMAPNIK=1"" ""$publish_mapnik_sdk"" && del /q packages\*.* && clean && scripts\build
+Z:\mb\windows-builds-32
+settings ""PACKAGEMAPNIK=1"" ""$publish_mapnik_sdk"" && del /q packages\*.* && clean && scripts\build
+Z:\mb\windows-builds-64
+" | Out-File -Encoding UTF8 Z:\wbs.cfg
+
+Get-ChildItem Env: | Out-File -Encoding utf8 Z:\env-vars.txt
