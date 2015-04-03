3697941753eb1a3a675b9cb9d0b11035bf03fca6
bergwerkgis <wb@bergwerk-gis.at>
2015-04-03 19:42:13 +0000

    [skip ci] [wip] libspatialite

---
+++ scripts/build_libspatialite.bat
@@ -0,0 +1,37 @@
+@echo off
+SETLOCAL
+SET EL=0
+echo ------ sqlite -----
+
+:: guard to make sure settings have been sourced
+IF "%ROOTDIR%"=="" ( echo "ROOTDIR variable not set" && GOTO DONE )
+
+cd %PKGDIR%
+CALL %ROOTDIR%\scripts\download libspatialite-%SPATIAL_LITE_VERSION%.tar.gz
+IF ERRORLEVEL 1 GOTO ERROR
+
+if EXIST libspatialite echo found extracted sources && GOTO SRCFOUND
+
+echo extracting
+CALL bsdtar xfz libspatialite-%SPATIAL_LITE_VERSION%.tar.gz
+rename libspatialite-%SPATIAL_LITE_VERSION% libspatialite
+IF ERRORLEVEL 1 GOTO ERROR
+
+:SRCFOUND
+
+cd libspatialite
+IF ERRORLEVEL 1 GOTO ERROR
+
+
+
+
+GOTO DONE
+
+:ERROR
+SET EL=%ERRORLEVEL%
+echo ----------ERROR sqlite --------------
+
+:DONE
+
+cd %ROOTDIR%
+EXIT /b %EL%
