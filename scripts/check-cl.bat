c5e2fce073161c4beece688ad2e37cf4d700683d
bergwerkgis <wb@bergwerk-gis.at>
2015-05-22 10:03:26 +0000

    [skip ci] fix bug in cl.exe version detection

--- scripts/check-cl.bat
+++ scripts/check-cl.bat
@@ -1,5 +1,5 @@
 @echo off
-FOR /F %%G IN (%1) DO cl /? 2>&1 | findstr /C:"Version %%G" > nul && goto FOUND
+FOR /F %%G IN ("%1") DO cl /? 2>&1 | findstr /C:"Version %%G" > nul && goto FOUND
 EXIT /B 1
 :FOUND
 EXIT /B 0
