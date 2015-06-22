41c63e4b7036ae6cb32a59899a7e3be3685eb77a
bergwerkgis <wb@bergwerk-gis.at>
2015-06-22 08:38:29 +0000

    [skip ci] node-gdal

--- scripts/build_node_gdal.bat
+++ scripts/build_node_gdal.bat
@@ -24,6 +24,13 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 if EXIST %USERPROFILE%\.node-gyp ddt /Q %USERPROFILE%\.node-gyp
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO downloading temporary node.exe to install deps ...
+SET SUFFIX=
+IF "%PLATFORMX%"=="x64" SET SUFFIX=x64/
+ECHO downloading node.exe https://mapbox.s3.amazonaws.com/node-cpp11/v%NODE_VERSION%/%SUFFIX%node.exe
+powershell Invoke-WebRequest "https://mapbox.s3.amazonaws.com/node-cpp11/v$env:NODE_VERSION/${env:SUFFIX}node.exe" -OutFile $env:PKGDIR\node-gdal\node.exe
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
 ECHO installing node-pre-gyp ...
 CALL npm install node-pre-gyp
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
@@ -35,7 +42,7 @@ CALL npm install
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 SETLOCAL EnableDelayedExpansion
-FOR %%N IN (0.10.33 0.11.14 0.12.0) DO (
+FOR %%N IN (0.10.33 0.11.14 0.12.2) DO (
 	ECHO about to build %%N %PLATFORMX%
 	CALL %ROOTDIR%\scripts\build_node_gdal-worker.bat %%N
 	ECHO ERRORLEVEL %%N %PLATFORMX% !ERRORLEVEL!
