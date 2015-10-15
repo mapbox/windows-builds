a41ae107d4d5f1defe2ca76eebd5aa035a5f5c7b
bergwerkgis <wb@bergwerk-gis.at>
2015-10-15 13:56:16 +0000

    [skip ci] node 4.x don't clone node-cpp11

--- scripts/build_node.bat
+++ scripts/build_node.bat
@@ -13,6 +13,7 @@ ECHO node major version^: %NODE_MAJOR%
 IF %NODE_MAJOR% GTR 0 ECHO node version greater than zero
 
 cd %PKGDIR%
+IF %NODE_MAJOR% GTR 0 GOTO NO_CPP11
 if NOT EXIST node-cpp11 git clone https://github.com/mapbox/node-cpp11.git
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
@@ -21,6 +22,8 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 git fetch -v
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+:NO_CPP11
 
 SET NODE_BRANCH=v%NODE_VERSION%-nodecpp11
 IF %NODE_MAJOR% GTR 0 SET NODE_BRANCH=v%NODE_VERSION%
