b7182e1c6977d1dc7465352e338a89fe780b30f5
bergwerkgis <wb@bergwerk-gis.at>
2016-09-07 14:25:48 +0000

    [skip ci] clean up freetype build script

--- scripts/build_freetype.bat
+++ scripts/build_freetype.bat
@@ -2,43 +2,29 @@
 SETLOCAL
 SET EL=0
 
-echo ----- freetype-----
+ECHO ~~~~~~~~~~~~~~~~~~~ %~f0 ~~~~~~~~~~~~~~~~~~~
 
 :: guard to make sure settings have been sourced
 IF "%ROOTDIR%"=="" ( echo "ROOTDIR variable not set" && GOTO DONE )
 
 cd %PKGDIR%
 CALL %ROOTDIR%\scripts\download freetype-%FREETYPE_VERSION%.tar.bz2
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-if EXIST freetype (
-  echo found extracted sources
-)
-
-if NOT EXIST freetype (
-  echo extracting
-  CALL bsdtar xfz freetype-%FREETYPE_VERSION%.tar.bz2
-  rename freetype-%FREETYPE_VERSION% freetype
-  IF ERRORLEVEL 1 GOTO ERROR
-)
-
-cd freetype
-IF ERRORLEVEL 1 GOTO ERROR
-
-if "%TARGET_ARCH%"=="64" (
-  echo.
-  ECHO "IF building x64 add platform to solution manually!"
-  ECHO.
-)
-
-::DebugInformationFormat
-::OldStyle = /Z7 (within file)
-::ProgramDatabase = /Zi (pdb)
-::EditAndContinue = /ZI
-REM ::/p:DebugInformation=EditAndContinue ^
+IF EXIST freetype ECHO found extracted sources && GOTO SRC_EXTRACTED
 
 
-ECHO building ...
+ECHO extracting && bsdtar xfz freetype-%FREETYPE_VERSION%.tar.bz2
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+ECHO renaming dir && rename freetype-%FREETYPE_VERSION% freetype
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+:SRC_EXTRACTED
+
+ECHO changing into freetype && CD freetype
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+ECHO building freetype ...
 msbuild ^
 .\builds\windows\vc2010\freetype.sln ^
 /p:ForceImportBeforeCppTargets=%ROOTDIR%\scripts\force-debug-information-for-sln.props ^
@@ -46,16 +32,16 @@ msbuild ^
 /m:%NUMBER_OF_PROCESSORS% ^
 /toolsversion:%TOOLS_VERSION% ^
 /p:BuildInParallel=true ^
-/p:Configuration="%BUILD_TYPE% Multithreaded" ^
+/p:Configuration="%BUILD_TYPE%" ^
 /p:Platform=%BUILDPLATFORM% ^
 /p:PlatformToolset=%PLATFORM_TOOLSET%
-IF ERRORLEVEL 1 GOTO ERROR
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-ECHO build ok
+ECHO build successful
 
 IF %BUILDPLATFORM% EQU x64 (
   IF %BUILD_TYPE% EQU Release (
-    CALL copy /Y objs\vc2010\x64\freetype%FREETYPE_VERSION_FILE%MT.lib freetype.lib
+    CALL copy /Y objs\vc2010\x64\freetype%FREETYPE_VERSION_FILE%.lib freetype.lib
     IF ERRORLEVEL 1 GOTO ERROR
   ) ELSE (
     CALL copy /Y objs\win64\vc2010\freetype%FREETYPE_VERSION_FILE%_D.lib freetype.lib
@@ -79,8 +65,9 @@ GOTO DONE
 
 :ERROR
 SET EL=%ERRORLEVEL%
-echo -------------FREETYPE ERROR -----------------
+ECHO ~~~~~~~~~~~~~~~~~~~ ERROR %~f0 ~~~~~~~~~~~~~~~~~~~
 
 :DONE
+ECHO ~~~~~~~~~~~~~~~~~~~ DONE %~f0 ~~~~~~~~~~~~~~~~~~~
 cd %ROOTDIR%
 EXIT /b %EL%
